0cdf2f4063baa933fb400d19de41cbf26ec1027e
Jelle De Loecker <jelle@kipdola.be>
2014-03-17 16:45:38 +0200

    Initial commit: add base alchemy files

---
+++ app/config/bootstrap.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Alchemy: Node.js MVC Framework
+ * Copyright 2013-2013
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright 2013-2013
+ * @since       0.0.1
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * Server
+ *
+ * The place where the magic starts
+ *
+ * Alchemy: a node.js framework
+ * Copyright 2013, Jelle De Loecker
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2013, Jelle De Loecker
+ * @link          
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+require('alchemymvc');
+
+alchemy.ready(function onAlchemyReady() {
+
+	// Do certain things when alchemy is ready
+
+});
